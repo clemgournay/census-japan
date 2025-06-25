546fd6ebeeb6047faaa546a89eb4990e76eedfbb
U-DESKTOP-QKC9GKG\Clément <clement.gournay-ext@epicea.eu>
2025-06-25 19:54:05 +0200

    Stylized filter card wrapper

--- app/components/parts/filter-card-wrapper/filter-card-wrapper.tsx
+++ app/components/parts/filter-card-wrapper/filter-card-wrapper.tsx
@@ -5,6 +5,9 @@ import FilterCard from '@components/parts/filter-card/filter-card';
 import CategorySelection from '@app/components/ui/category-selection/category-selection';
 import PrefectureSelection from '@app/components/ui/prefecture-selection/prefecture-selection';
 
+// Styles
+import styles from './filter-card-wrapper.module.scss';
+
 type Props = {
   prefCodes: Array<number>;
   category: string;
@@ -14,14 +17,20 @@ export default function FilterCardWrapper({prefCodes, category}: Props) {
 
   return (
     <FilterCard>
-      <h4>人口カテゴリー</h4>
-      <Suspense>
-        <CategorySelection category={category} />
-      </Suspense>
-      <h4>都道府県</h4>
-      <Suspense>
-        <PrefectureSelection prefCodes={prefCodes} />
-      </Suspense>
+      <div className={styles.filters}>
+        <div className={styles.filter}>
+          <h4>人口カテゴリー</h4>
+          <Suspense>
+            <CategorySelection category={category} />
+          </Suspense>
+        </div>
+        <div className={styles.filter}>
+          <h4>都道府県</h4>
+          <Suspense>
+            <PrefectureSelection prefCodes={prefCodes} />
+          </Suspense>
+        </div>
+      </div>
     </FilterCard>
   )
 }
